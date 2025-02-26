f7f21c666899b9a5783b4f657835090b6fe605ea
RoxanaG <roxanagrzywacz@gmail.com>
2025-02-26 15:46:28 +0100

    Code update

--- data/import_json_to_mysql.py
+++ data/import_json_to_mysql.py
@@ -1,40 +1,40 @@
 import json
 import mysql.connector
 
-# Połącz się z bazą danych MySQL
+# Connect to the MySQL database
 conn = mysql.connector.connect(
     host="localhost",
-    user="Roxana",  # Zmień na swoją nazwę użytkownika
-    password="TMJFMR6@!h1",  # Zmień na swoje hasło
-    database="VetDataBase"  # Zmień na nazwę swojej bazy danych
+    user="name",  # Change to your username
+    password="password",  # Change to your password
+    database="dataBaseName"  # Change to your database name
 )
 cursor = conn.cursor()
 
-# Wczytaj plik JSON
+# Load the JSON file
 with open("vet_patients_data.json", "r", encoding="utf-8") as file:
     data = json.load(file)
 
-# Słownik do przechowywania właścicieli (aby uniknąć duplikatów)
+# Dictionary to store owners (to avoid duplicates)
 owners_dict = {}
 
-# Wstawianie danych właścicieli i pacjentów
+# Insert owners and patients data
 for pet in data:
     owner_name = pet["owner"]["name"]
     owner_phone = pet["owner"]["phone"]
 
-    # Sprawdzenie, czy właściciel już istnieje
+     # Check if the owner already exists
     if (owner_name, owner_phone) not in owners_dict:
         cursor.execute("INSERT INTO owner (name, phone) VALUES (%s, %s)", (owner_name, owner_phone))
-        owners_dict[(owner_name, owner_phone)] = cursor.lastrowid  # Pobranie ID nowego właściciela
+        owners_dict[(owner_name, owner_phone)] = cursor.lastrowid  # Get the ID of the new owner
     owner_id = owners_dict[(owner_name, owner_phone)]
 
-    # Wstawienie pacjenta
+    # Insert the pet data
     cursor.execute("""
         INSERT INTO patients (name, species, breed, age, weight_kg, owner_id)
         VALUES (%s, %s, %s, %s, %s, %s)
     """, (pet["name"], pet["species"], pet["breed"], pet["age"], pet["weight_kg"], owner_id))
 
-# Zatwierdzenie zmian i zamknięcie połączenia
+# Commit the changes and close the connection
 conn.commit()
 cursor.close()
 conn.close()
